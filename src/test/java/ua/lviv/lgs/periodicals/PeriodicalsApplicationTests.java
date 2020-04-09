2296c8b7973f1df75acb7cf2671fb6620111e49a
taras2480 <aviator2480@gmail.com>
2020-04-10 00:59:22 +0300

    add registry and Login form

---
+++ src/test/java/ua/lviv/lgs/periodicals/PeriodicalsApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.lviv.lgs.periodicals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PeriodicalsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
